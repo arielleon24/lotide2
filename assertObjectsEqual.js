ba9d49c4f9da845bcf72f1aee39535122ec5b7df
Ariel Leon <arielleon@MacBook-Pro.local>
2020-10-28 18:42:46 -0400

    assertObjectsEqual.js implemented

---
+++ assertObjectsEqual.js
@@ -0,0 +1,35 @@
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (let key in object1) {
+      if (object1[key] !== object2[key]) {
+        return false
+      }
+    }
+    return true
+  }
+    return false
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; // IMPORTS UTIL LIBRARY WHICH ALLOWS TO FIX THE OUTPUT DUE TO INSPECT FUNCTION
+  if (Object.keys(actual).length !== Object.keys(expected).length) {
+    console.log(`😡😡😡 Assertion Failed: ${inspect(actual)} & ${inspect(expected)} do not have the same #of keys`);
+    return false;
+  } else {
+  }
+  for (let key in actual) {
+    if (actual[key] !== expected[key]) {
+      console.log(`😡😡😡 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+      return false;
+    }
+  }
+  console.log(`😃😃😃 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+};
+
+const obtest1 = {a: 1, b: 2, c: 3};
+const obtest10 = {a: 1, b: 2, c: 3};
+const obtest2 = {a: 1, b: 2, c: 3, d:3};
+const obtestA = {a: 1, b: "2", c: 3};
+assertObjectsEqual(obtest1, obtestA); // should fail
+assertObjectsEqual(obtest1, obtest10); // should pass
+assertObjectsEqual(obtest1, obtest2); // different length
