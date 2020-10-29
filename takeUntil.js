5c02644dd3790b4099fbb55051cd438fbb8e5895
Ariel Leon <arielleon@MacBook-Pro.local>
2020-10-29 15:47:02 -0400

    takeUntil Implemented

--- takeUntil.js
+++ takeUntil.js
@@ -22,5 +22,9 @@ console.log(results1);
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// X in the line below refers to the item with which we are testing the condition. As long as when X is strictly equal to ','
+// The function (x => x === ',') will test to true. 
+// X must be given a value when passed into takeUntil and that value is taken from iterating over the array.
+
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
