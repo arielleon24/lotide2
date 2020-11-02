bee53e9b7c88aa23394fd386286d6082d56a5196
Ariel Leon <arielleon@MacBook-Pro.local>
2020-11-02 14:30:23 -0500

    eqArrays refactored

---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require("../assertEqual");
+const eqArrays = require("../eqArrays")
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
