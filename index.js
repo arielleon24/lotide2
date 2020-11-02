70093dbe329cde2f51789fffaa3f957d1515fd48
Ariel Leon <arielleon@MacBook-Pro.local>
2020-11-02 17:14:00 -0500

    Index created to refer to all functions

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+
+module.exports = {
+  head,
+  tail,
+  middle, 
+  assertArraysEqual, 
+  eqArrays, 
+  assertEqual
+};
