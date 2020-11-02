8f54b6071437d32966129d58824877448db9d463
Ariel Leon <arielleon@MacBook-Pro.local>
2020-11-02 16:54:33 -0500

    Tail and Middle Mocha Testing implemented

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,30 @@
+const { assert } = require("chai");
 const assertArraysEqual = require("../assertArraysEqual");
 const eqArrays = require("../eqArrays");
 const middle = require("../middle")
 
 
-//TEST CODE // 
-assertArraysEqual(middle([1, 2, 3]), [2]);// should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [5]); // should FAIL
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [5]); // should FAIL
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // should PASS
+// //TEST CODE // 
+// assertArraysEqual(middle([1, 2, 3]), [2]);// should PASS
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // should PASS
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [5]); // should FAIL
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [5]); // should FAIL
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // should PASS
+
+
+// MOCHA AND CHAI TEST 
+
+describe('Middle Test', () => {
+  it('Returns [2] from [1, 2 , 3]',() => {
+    assert.deepEqual(middle([1, 2 ,3]), [2]);
+  });
+
+  it('Returns [3] from [1, 2 , 3, 4, 5]',() => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it('Returns [2, 3] from [1, 2 , 3, 4]',() => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,24 @@
+const { assert } = require("chai");
 const tail = require("../tail");
 const assertEqual = require("../assertEqual");
-const assertArraysEqual = require("../assertArraysEqual")
+const assertArraysEqual = require("../assertArraysEqual");
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-result = tail(words); // no need to capture the return value since we are not checking it
-assertArraysEqual(tail(words), ["Lighthouse", "Labs"]); /// will fail unless you use assertArraysEqual
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// // Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// result = tail(words); // no need to capture the return value since we are not checking it
+// assertArraysEqual(tail(words), ["Lighthouse", "Labs"]); /// will fail unless you use assertArraysEqual
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+//// MOCHA AND CHAI 
+
+describe("Tail Function", () => {
+  it("Return ['lighthouse', 'labs'] with ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'lighthouse', 'labs']), [ 'lighthouse', 'labs']);
+  });
+
+  it("Return ['2', '3'] from ['1', '2', '3']", () => {
+    assert.deepEqual(tail(['1', '2', '3']), [ '2', '3']);
+  });
+
+});
